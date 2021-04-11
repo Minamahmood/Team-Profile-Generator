003df530736e8c94153f2dc7e53dfdd7a2dc1eb6
minamahmood83 <minamahmood83@gmail.com>
2021-04-11 13:11:23 -0700

    finsh engineert test value

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -2,6 +2,11 @@ const Engineer = require("../lib/engineer");
 
 test("can set GitHub account via constructor", () => {
     const testValue = "GitHubUser";
-    const e = new Engineer("mm", "fake@fake.com", "GitHubUser");
-    expect(e.getRole()).tobe(testValue);
+    const e = new Engineer("mmm", 1, "fake@fake.com", "GitHubUser");
+    expect(e.getRole()).toBe(testValue);
+});
+test("can get GitHub username via get GitHub()", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("mmm", 1, "fake@fake.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
 });
