bbc66ba8f4d2f6e76e521c51e4a8b1f3f649cdcd
minamahmood83 <minamahmood83@gmail.com>
2021-04-11 21:54:04 -0700

    create engineer.test.js question

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -4,3 +4,7 @@ test("Get name", () => {
     const engineer = new Engineer("name");
     expect(engineer.getName());
 });
+test("Get id", () => {
+    const engineer = new Engineer("id");
+    expect(engineer.getId());
+});
