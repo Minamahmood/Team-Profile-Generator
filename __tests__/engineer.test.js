8747c96e0378123ee367543019961f9afe960032
minamahmood83 <minamahmood83@gmail.com>
2021-04-11 21:50:27 -0700

    update engineer.test.js

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -1,12 +1,1 @@
 const Engineer = require("../lib/engineer");
-
-test("can set GitHub account via constructor", () => {
-    const testValue = "GitHubUser";
-    const e = new Engineer("name", "fake@fake.com", "GitHubUser");
-    expect(e.getGithub()).toBe(testValue);
-});
-test("can get GitHub username via get GitHub()", () => {
-    const testValue = "GitHubUser";
-    const e = new Engineer("name", "fake@fake.com", testValue);
-    expect(e.getGithub()).toBe(testValue);
-});
