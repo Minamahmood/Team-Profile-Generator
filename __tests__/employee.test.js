98007340cf4b92c9b8d26b637587fbd42e26b4ef
minamahmood83 <minamahmood83@gmail.com>
2021-04-11 22:13:25 -0700

    add employee link to app.js

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -3,3 +3,6 @@ test("dose my test work", () => {
     const yes = true;
     expect(yes).toBe(true);
 });
+// const Manager = require("../lib/manager");
+// const Engineer = require("../lib/engineer");
+// const Intern = require("../lib/intern");
--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputpath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
+const Employee = require("./lib/employee");
 
 const team = [];
 addToTeam();
